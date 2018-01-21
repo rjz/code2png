ee9c3783f4675a5602a07835e46cd58578aed887
rjz <rj@rjzaworski.com>
2018-01-21 10:24:05 -0800

    Adds language flag

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+
 const render = require('./src/render');
 const capture = require('./src/capture');
 
@@ -7,11 +9,15 @@ const DEFAULT_LANGUAGE = 'javascript';
 const DEFAULT_THEME = 'okaidia';
 
 module.exports = function code2png (code, opts) {
-  const content = render({
-    code,
-    theme: opts.theme || DEFAULT_THEME,
-    language: opts.language || DEFAULT_LANGUAGE,
-  });
+
+  const theme = opts.theme || DEFAULT_THEME;
+  const language = opts.language || DEFAULT_LANGUAGE;
+
+  if (!fs.existsSync(render.languageFile(language))) {
+    throw new Error(`Unknown language '${language}'`);
+  }
+
+  const content = render({ code, theme, language });
 
   return capture(content);
 };
